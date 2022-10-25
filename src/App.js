fc25d1ce245d578ebccaf03b31019589f623220b
Natan Ribeiro <natan.ribeiro@precode.com.br>
2022-10-25 17:44:06 -0300

    adding the qrcode on input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { useState } from 'react';
+import QRCode from 'react-qr-code';
 import './App.css';
 
 function App() {
+   const [link, setLink] = useState('') 
+
   return (
     <div className="container">
+      {/* example inset www.google.com on value */}
+      <QRCode value={link}/>
+
       <input className="input" placeholder="digite seu link..."/>
     </div>
   );
