b5b9b6696489addfaf60c1e944f96afc41ae425e
Natan Ribeiro <natan.ribeiro@precode.com.br>
2022-10-27 09:16:32 -0300

    feat: download the generated qrcode img

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import { useState } from 'react';
 import QRCode from 'react-qr-code';
+import QRCodeLink from 'qrcode';
 import './App.css';
 
 function App() {
    const [link, setLink] = useState('') 
+   const [qrCodeLink, setQrCodeLink] = useState('')
+
+   function handleGenerate(link_url){
+      QRCodeLink.toDataURL(link_url,{
+        width: 300,
+        margin: 3,
+      }, function (err, url){
+        setQrCodeLink(url);
+      })
+   }
 
    function handleQrcode(e){
     setLink(e.target.value);
+    handleGenerate(e.target.value);
    }
 
   return (
@@ -15,6 +27,9 @@ function App() {
       <QRCode value={link}/>
 
       <input className="input" placeholder="digite seu link..." value={link} onChange={ (e) => handleQrcode(e) }/>
+
+      <a href={qrCodeLink} download={`qrcode.png`}>Baixar QrCode</a>
+
     </div>
   );
 }
